bee655b5e33d939460cea5762b2d37b4f730bb33
Somnath M <somnath.muthukumaran@gmail.com>
2025-08-03 22:06:58 +0530

    linked list with and without tail added

---
+++ ds/linked-list/single/index.ts
@@ -0,0 +1,45 @@
+class Node<T = any> {
+  data!: T;
+  next: Node<T> | null = null;
+  constructor(value: T) {
+    this.data = value;
+    this.next = null;
+  }
+}
+export class SingleLinkedListWithoutTail<T = any> {
+  head!: Node<T> | null;
+  constructor(value: T) {
+    this.head = new Node(value);
+  }
+
+  append(value: T) {
+    // time complexity is high due to traversal
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+}
+
+export class SingleLinkedListWithTail<T = any> {
+  head!: Node<T> | null;
+  tail!: Node<T> | null;
+  constructor(value: T) {
+    const node = new Node(value);
+    this.head = node;
+    this.tail = node;
+  }
+
+  append(value: T) {
+    // reduced time complexity due to the tail O(1)
+    const newNode = new Node(value);
+    this.tail!.next = newNode;
+    this.tail = newNode;
+  }
+}
