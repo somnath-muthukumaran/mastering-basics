55799d07faa73a234daa2d606dae45d541d01eaf
Somnath M <somnath.muthukumaran@gmail.com>
2025-08-23 22:06:49 +0530

    sliding window added

--- ds/array/sliding-window.ts
+++ ds/array/sliding-window.ts
@@ -1,3 +1,36 @@
+/* conventional sliding window
+ * function slidingWindow(arr, k) {
+ *  for (let i = 0; i <= arr.length - k; i++) {
+ *    let window = arr.slice(i, i + k); // current window
+ *    console.log("Window:", window);
+ *  }
+ *}
+ *slidingWindow([1, 2, 3, 4, 5, 6], 3);
+ */
+
+/**
+ * O(n.k) and we have better way to solve this
+ */
+// function  firstNegativeNumberInEveryWindowOfSizeK() {
+//     const k = 3
+//     const targetArr = [-1,2,-3,0,2,3,7,-9]
+
+//     const len = targetArr.length;
+//     let window = targetArr.slice(0, k);
+//     const result = [];
+//     let firstNeg = window.find(x => x < 0);
+//   result.push(firstNeg !== undefined ? firstNeg : 0);
+//     for (let i = k; i < len; i++) {
+//       let entry = targetArr[i];
+//       window.shift();
+//       window.push(entry);
+//       const firstNeg = window.find((x) => x < 0);
+//       result.push(firstNeg !== undefined ? firstNeg : 0);
+//       console.log("Window:", window, "Entry:", entry);
+//     }
+//     console.log("result",result)
+//   }
+
 class SlindingWindow {
   targetArr!: number[];
   targetStr!: string;
@@ -5,6 +38,7 @@ class SlindingWindow {
     this.targetArr = arr;
     this.targetStr = str;
   }
+
   /**
    * Problem 1: Maximum Sum Subarray of Size K
    */
@@ -13,17 +47,73 @@ class SlindingWindow {
   /**
    *  Problem 2: First Negative Number in Every Window of Size K
    */
-  firstNegativeNumberInEveryWindowOfSizeK() {}
+  firstNegativeNumberInEveryWindowOfSizeK(k: number) {
+    const arr = this.targetArr;
+    const queue = [];
+    const result = [];
+    for (let i = 0; i < k; i++) {
+      if (arr[i] < 0) queue.push(i);
+    }
+    result.push(queue.length ? arr[queue[0]] : 0);
+
+    for (let i = k; i < arr.length; i++) {
+      5 - 3;
+      while (queue.length > 0 && queue[0] <= i - k) {
+        queue.shift();
+      }
+      if (arr[i] < 0) queue.push(i);
+
+      result.push(queue.length ? arr[queue[0]] : 0);
+    }
+  }
 
   /**
    *  Problem 3: Count Distinct Elements in Every Window of Size K
+   * frequency map
+   * set won't workout if there's a repeatitive element in the array hence map
    */
-  countDistinctElementsInEveryWindowOfSizeK() {}
+  countDistinctElementsInEveryWindowOfSizeK(k: number) {
+    const arr = this.targetArr;
+    const frequency = new Map();
+    const result = [];
+    for (let i = 0; i < k; i++) {
+      const count = frequency.get(arr[i]) || 0;
+      frequency.set(arr[i], count + 1);
+    }
+    result.push(frequency.size);
+
+    for (let i = k; i < arr.length; i++) {
+      const exit = arr[i - k];
+      const exitCount = frequency.get(exit);
+      frequency.set(exit, exitCount - 1);
+      if (frequency.get(exit) <= 0) {
+        frequency.delete(exit);
+      }
+      const count = frequency.get(arr[i]) || 0;
+      frequency.set(arr[i], count + 1);
+      result.push(frequency.size);
+    }
+    console.log(result);
+  }
 
   /**
    * Problem 1: Longest Substring Without Repeating Characters
    */
-  longestSubStringWithoutRepeatingCharacters() {}
+  longestSubStringWithoutRepeatingCharacters() {
+    const input = "abcabcbb";
+    let maxSubLen = 0;
+    let left = 0;
+    const seenList = new Set();
+    for (let right = 0; right < input.length; right++) {
+      while (seenList.has(input[right])) {
+        seenList.delete(input[left]);
+        left++;
+      }
+      seenList.add(input[right]);
+      maxSubLen = Math.max(maxSubLen, right - left + 1);
+    }
+    return maxSubLen;
+  }
 
   /**
    * Problem 2: Smallest Subarray with a Sum ≥ S
