78131fcb44aed69449380dfeaf6f1e0e0de984ae
somnath-muthukumaran <somnath@edvoy.com>
2025-09-01 12:42:19 +0530

    added updates

--- ds/array/runner.ts
+++ ds/array/runner.ts
@@ -1,24 +1,14 @@
-function mergeSorted(numsM: number[], m: number, numsN: number[], n: number) {
-  let last = numsM.length - 1;
-  let targetMax = m - 1;
-  let valueMax = n - 1;
-
-  while (last >= 0) {
-    if (targetMax >= 0 && valueMax >= 0)
-      if (numsM[targetMax] > numsN[valueMax]) {
-        numsM[last] = numsM[targetMax];
-        targetMax--;
-      } else {
-        numsM[last] = numsN[valueMax];
-        valueMax--;
-      }
-    last--;
+function isPalindrome(s: string): boolean {
+  let left = 0;
+  let right = s.length - 1;
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
   }
+  return true;
 }
 
-const target = [1, 2, 3, 0, 0, 0];
-const values = [3, 4, 5];
-const m = 3;
-const n = 3;
-mergeSorted(target, m, values, n);
-console.log("target", target);
+console.log(isPalindrome("racecar"));
